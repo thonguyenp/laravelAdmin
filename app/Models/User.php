0cce70718870e816e988bf94596ed9d19b324094
thonguyenp <nphuoctho2406@gmail.com>
2025-09-18 14:46:40 +0700

    Hoàn thành việc thêm cột role cho bảng users

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role', //mỗi lần thêm cột thì phải thêm thuộc tính vào fillable
     ];
 
     /**

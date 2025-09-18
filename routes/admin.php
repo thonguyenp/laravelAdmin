54b82b5c5b1557bdf7db10909fa6c9da9d44b60f
thonguyenp <nphuoctho2406@gmail.com>
2025-09-18 14:40:05 +0700

    Hoàn thành xác thực người dùng bằng middleware auth

--- routes/admin.php
+++ routes/admin.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('admin')->name('admin.')->group(function(){
+Route::middleware('auth')->prefix('admin')->name('admin.')->group(function(){
     Route::get('dashboard', [AdminController::class, 'index'])->name('dashboard');  //admin.dashboard
 });

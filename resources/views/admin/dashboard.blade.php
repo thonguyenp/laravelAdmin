30109185d93559a09d6012cb004b741f05028f5e
thonguyenp <nphuoctho2406@gmail.com>
2025-09-18 14:38:19 +0700

    Vào admin dashboard thành công

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main')
+
+@section('content')
+    <h1>This is admin dashboard</h1>
+@endsection
--- routes/admin.php
+++ routes/admin.php
@@ -1,3 +1,8 @@
 <?php
 
-?>
+use App\Http\Controllers\AdminController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('admin')->name('admin.')->group(function(){
+    Route::get('dashboard', [AdminController::class, 'index'])->name('dashboard');  //admin.dashboard
+});
